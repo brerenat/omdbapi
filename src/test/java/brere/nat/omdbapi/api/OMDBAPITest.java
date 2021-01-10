fe887b24b3fe17694ece90a3fd0dd021d3b43481
Ginge117 <nathan-bf@live.co.uk>
2021-01-10 15:56:44 +0100

    update to test

--- src/test/java/brere/nat/omdbapi/api/OMDBAPITest.java
+++ src/test/java/brere/nat/omdbapi/api/OMDBAPITest.java
@@ -1,6 +1,6 @@
 package brere.nat.omdbapi.api;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
